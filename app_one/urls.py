17ccce323637c995e747252f923e787e1a446631
David Kavanaugh <davidkavanaugh.dev@outlook.com>
2020-10-28 18:33:05 -0700

    feat: static and templates

--- app_one/urls.py
+++ app_one/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('route-one', views.route_one),
+    path('route-two/<str:name>', views.route_two),
+    path('route-three', views.route_three),
 ]
--- app_one/views.py
+++ app_one/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.http import JsonResponse
+from time import gmtime, strftime
 
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request, "index.html", context)
+
+
+def route_one(request):
+    return redirect("/")
+
+
+def route_two(request, name):
+    context = {
+        "name": name
+    }
+    return render(request, "template2.html", context)
+
+
+def route_three(request):
+    return JsonResponse({"content": "lorem ipsum", "status": True})
